25488a5957e2ba8e62213b53bdb040d874dc36ca
J0bot <jojo64364@gmail.com>
2024-08-29 11:20:48 +0200

    ajout de plusieurs trucs

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,44 @@
+/**
+ * @file hello.cpp
+ * @author J0bot
+ * @brief just a random program
+ * @version 0.1
+ * @date 2024-08-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
 #include <iostream>
 
+/**
+ * @brief namespace sdt pour eviter les sdt::cout etc...
+ * 
+ */
 using namespace std;
 
+int my_func(int);
+
+
+
+/**
+ * @brief fait la puissance au carré d'un nombre
+ * 
+ * @param a le nombre 
+ * @return int retourne la puissance au carré de a
+ */
+int my_func(int a){
+    return a*a;
+}
+
+/**
+ * @brief fonction main, ne sert à rien
+ * 
+ * @return int 
+ */
 int main(){
     cout << "Hello World" <<endl;
+    cout << my_func(3) <<endl;
     return 0;
 }
