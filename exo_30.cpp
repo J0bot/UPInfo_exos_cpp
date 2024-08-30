2d37c9165e12266b432ba168172f35322080b453
J0bot <jojo64364@gmail.com>
2024-08-30 10:39:26 +0200

    AHH

--- exo_30.cpp
+++ exo_30.cpp
@@ -13,16 +13,40 @@
 
 using namespace std;
 
+/**
+ * @brief La fonction main calcule le colume d'un cône
+ *
+ * @return int
+ */
 int main()
 {
     cout << "*** Calcul du volume d'un cône ***" << endl;
+    /**
+     * @brief variables de rayon et hauteur en float
+     *
+     */
     float rayon = 0;
     float hauteur = 0;
+
+    /**
+     * @brief Input des données dans les variables
+     *
+     */
     cout << "Donne le rayon [m] : ";
     cin >> rayon;
     cout << "Donne la hauteur [m] : ";
     cin >> hauteur;
+
+    /**
+     * @brief Calcul du volume en float, utilisation de la lib cmath pour M_PI et pow()
+     *
+     */
     float volume = M_PI * pow(rayon, 2) * hauteur / 3;
+
+    /**
+     * @brief Affichage du volume
+     *
+     */
     cout << "Volume [m3] : " << volume << endl;
     return 0;
 }
